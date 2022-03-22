dd9484cd2c0cc28654789c606d6be640cde055be
Aleksa Stancev <a.stancev@elfak.rs>
2022-03-22 17:07:03 +0100

    Sertifikat i kestrel podesavanje

--- Aplikacija/Server/Server/Program.cs
+++ Aplikacija/Server/Server/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Server
@@ -18,9 +19,32 @@ namespace Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureServices((context, services) =>
+            {
+                HostConfig.CertPath = context.Configuration["CertPath"];
+                HostConfig.CertPassword = context.Configuration["certpassword"];              
+                HostConfig.HostDnsEntry = context.Configuration["HostDnsEntry"];
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.ConfigureKestrel(opcije =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    var host = Dns.GetHostEntry(HostConfig.HostDnsEntry);
+                    opcije.Listen(host.AddressList[0], 5000);
+                    opcije.Listen(host.AddressList[0], 5001, listenOpcije =>
+                    {
+                        listenOpcije.UseHttps(HostConfig.CertPath, HostConfig.CertPassword);
+                    });
                 });
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+
+    public static class HostConfig
+    {
+        public static string CertPath { get; set; }
+        public static string CertPassword { get; set; }
+        public static int MaxRequestBodySize { get; set; }
+        public static string HostDnsEntry { get; set; }
     }
 }
